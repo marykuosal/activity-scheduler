68cf8ce38c6778192a97964520098bbbf4d55abb
Mariya Galasyuk <mariyagalasyuk@Mariyas-MacBook-Pro.local>
2022-06-01 10:00:38 +0200

    Structured the folder and created a client

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,12 +10,12 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors())
 app.use(activityRoutes)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ActivitiesCluster.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 const connectOptions = { useNewUrlParser: true, useUnifiedTopology: true }
 // mongoose.set("", false)
 
 mongoose
-  .connect(uri, Option: connectOptions)
+  .connect(uri)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server running on http://localhost:${PORT}`)
